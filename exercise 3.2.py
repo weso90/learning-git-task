eb51bc42a11ba378279a1781419f70759a6afa1c
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-23 14:46:39 +0100

    delete big letter in product names

--- exercise 3.2.py
+++ exercise 3.2.py
@@ -5,6 +5,6 @@ shopping_list = {
 }
 
 for i, j in shopping_list.items():
-    print(f"Idę do {i.capitalize()} i kupuję tam: {[j.capitalize() for j in j]}")
+    print(f"Idę do {i.capitalize()} i kupuję tam: {j}")
 
 print(f"W sumie kupuję {[sum(len(i) for i in shopping_list.values())]} towarów")

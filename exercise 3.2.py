23b48bde2e55084fee3bfab501adb52858c48fd5
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-23 13:06:45 +0100

    added dictionary shopping_list

--- exercise 3.2.py
+++ exercise 3.2.py
@@ -2,6 +2,3 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
-
-for i, j in shopping_list.items():
-    print(f"Idę do {i} i kupuję tam: {j}")

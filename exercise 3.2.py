1b5d314157eb869d801d99b671651938b4105f3d
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-23 13:08:56 +0100

    added for loop to iterate shopping_list

--- exercise 3.2.py
+++ exercise 3.2.py
@@ -2,3 +2,6 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+for i, j in shopping_list.items():
+    print(f"Idę do {i} i kupuję tam: {j}")

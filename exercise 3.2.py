9ee5a240eaff6f567057286db638bc71e2eac45a
Maksym Wesolowski <weso.pl@gmail.com>
2025-03-23 13:03:51 +0100

    Init project

---
+++ exercise 3.2.py
@@ -0,0 +1,7 @@
+shopping_list = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+
+for i, j in shopping_list.items():
+    print(f"Idę do {i} i kupuję tam: {j}")
